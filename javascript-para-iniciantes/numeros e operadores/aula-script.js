159ecaaee0606a7873e28b4d4da31e934ffb2fca
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-06-22 11:57:47 -0300

    new

---
+++ javascript-para-iniciantes/numeros e operadores/aula-script.js
@@ -0,0 +1,15 @@
+var teste = 21e10; //'e' adiciona uma quantidade x de zeros
+console.log(teste);
+
+var teste2 = 2e-2; //0.02
+console.log(teste2);
+
+// '10' + 10 = '1010' -> concatena
+// '10' - 10 = '0' -> converte mas retorna string
+
+var idade = '28';
+var somaIdade = 5;
+
+// idade + somaIdade -> '33' (string)
+// +idade + somaIdade -> 33 (number)
+// -idade + somaIdade -> -33 (number)
---
+++ javascript-para-iniciantes/numeros e operadores/script.js
@@ -0,0 +1,8 @@
+//Exercício
+
+//Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade;
+var pesoPorDois = (+numero / 2) + unidade;
+console.log(pesoPorDois)
