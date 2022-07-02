89125dfb2c55a5df18c18b019e936e46222ad1ee
Dayanne <81201377+dayannebugarim@users.noreply.github.com>
2022-07-02 20:57:55 -0300

    new

---
+++ javascript-para-iniciantes/arrays-e-loops/aula-script.js
@@ -0,0 +1,7 @@
+var videoGames = ['Switch', 'PS4', 'XBOX', '3DS'];
+
+var frutas = ['Banana', 'Pera', 'Maçã', 'Abacaxi', 'Uva'];
+
+frutas.forEach(function(fruta, index) {
+    console.log(fruta, index);
+}); //o parametro passado na function do forEach dinamicamente retorna o(s) item(s) do array
---
+++ javascript-para-iniciantes/atribuicao-e-ternario/aula-script.js
@@ -0,0 +1,11 @@
+var idade = 19;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber);
+// condição ? true : false
+
+var possuiFaculdade = true;
+if(possuiFaculdade)
+    console.log('Sim, possui');
+else
+    console.log('Não possui');
+// if/else funciona sem {} se possuir apenas uma linha de comando
