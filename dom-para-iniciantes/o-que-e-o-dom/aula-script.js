733b4f3ccbfcdcae17c503ba3a1556d002919183
Dayanne <81201377+dayannebugarim@users.noreply.github.com>
2022-07-03 22:29:18 -0300

    new

---
+++ dom-para-iniciantes/o-que-e-o-dom/aula-script.js
@@ -0,0 +1,14 @@
+window.alert('Isso mesmo');
+
+const href = window.location.href;
+
+console.log(href);
+
+const titulo = document.querySelector('h1');
+
+titulo.innerText; // retorna o texto
+titulo.classList; // retorna as classes
+titulo.id; // retorna o id
+titulo.offsetHeight; // retorna a altura do elemento;
+
+titulo.addEventListener('click', callback); // ativa a função callback ao click no titulo
---
+++ dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -0,0 +1,15 @@
+// Retorne o url da página atual utilizando o objeto window
+
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo');
+console.log(elementoAtivo);
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
+// Retorne a largura da janela
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
